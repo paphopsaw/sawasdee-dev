986a41dee1c40aaa0e34148cf9ef3a001c58f149
Paphop Sawasdee <paphop.staw@gmail.com>
2025-02-16 23:28:20 +0900

    Remove unused

--- docs/.vuepress/layouts/Articles.vue
+++ docs/.vuepress/layouts/Articles.vue
@@ -3,9 +3,7 @@ import { useBlogType, useBlogCategory } from "@vuepress/plugin-blog/client";
 import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue";
 import ArticleList from "../components/ArticleList.vue";
 import TagList from "../components/TagList.vue";
-import { RouteLink, useRoute } from "vuepress/client";
 
-const route = useRoute();
 const articles = useBlogType("articles");
 const tagMap = useBlogCategory("tags");
 </script>
